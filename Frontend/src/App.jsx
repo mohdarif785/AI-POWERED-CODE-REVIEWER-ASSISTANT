ec9b98925875ee355d4c4d2f87e2170a33986ec5
Mohd Arif <ma9918127@gmail.com>
2025-03-22 16:22:49 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -30,7 +30,7 @@ System.out.println(a+b);
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-powered-code-reviewer-assistant.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
