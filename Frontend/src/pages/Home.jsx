b15b1e7c87a61b03d909186d41228ce737729b47
Mohd Arif <ma9918127@gmail.com>
2025-09-21 12:22:26 +0530

    done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../contexts/AuthContext';
 import { 
   CodeBracketIcon, 
   SparklesIcon, 
-  ShieldCheckIcon, 
+  ShieldCheckIcon,
   RocketLaunchIcon,
   CheckCircleIcon,
   ArrowRightIcon
